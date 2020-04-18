b880c3a9adc57a43d6e451bedd566b78d7ad9ad6
Kazutaka Ohashi <ichitadon@xenodata-lab.com>
2020-04-18 22:43:02 +0900

    add materials for pdf output that Japanese sentences and asciidoctor-mathematical

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -20,4 +20,4 @@ CURRENT_PATH=`pwd`
 # -r, --require = LIBRARY
 
 asciidoctor -r asciidoctor-mathematical -a mathematical-format=svg -a mathematical-inline=true -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a imagesdir=${CURRENT_PATH}/images  -r asciidoctor-diagram   index.adoc
-asciidoctor-pdf -r asciidoctor-mathematical -a mathematical-format=svg -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images -a scripts@=cjk -a allow-uri-read index.adoc
+asciidoctor-pdf -r asciidoctor-mathematical -a mathematical-format=svg -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o sample.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk    -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram -r ${CURRENT_PATH}/configs/config.rb   -a allow-uri-read index.adoc
